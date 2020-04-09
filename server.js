8cbe25d3eb3597fe4f195bab5f62259284d2caa9
Marie Antons <maantons@mail.fhsu.edu>
2020-04-09 16:26:14 -0500

    added get location from cache

--- server.js
+++ server.js
@@ -44,27 +44,54 @@ app.get('/trails', getTrails);
 // app.get('/movies', );
 // app.get('/yelp', );
 
+function getLocationFromCache(city) {
+  const SQL = `
+  SELECT *
+  FROM Locations
+  WHERE search_query = $1
+  LIMIT 1
+  `;
+  const parameters = [city];
+  return client.query(SQL, parameters);
+}
+
 // function is storing this into the locations table
-function setLocationInCache(location){
-  const { search_query, formatted_query, latitude, longitude} = location;
+function setLocationInCache(location) {
+  const { search_query, formatted_query, latitude, longitude } = location;
   const SQL = `
   INSERT INTO Locations (search_query, formatted_query, latitude, longitude)
   VALUES ($1, $2, $3, $4)
   RETURNING *
   `;
   const parameters = [search_query, formatted_query, latitude, longitude];
-  client.query(SQL,parameters)
-  .then(result => {
-    console.log('Cache Location', result);
-  })
-  .catch(err => {
-    console.error('Failed to cache location', err);
-  })
+  client.query(SQL, parameters)
+    .then(result => {
+      console.log('Cache Location', result);
+    })
+    .catch(err => {
+      console.error('Failed to cache location', err);
+    })
   client.query(SQL)
 }
 // Route Handler: location
 function locationHandler(request, response) {
   const city = request.query.city;
+
+
+  //returning promise
+  getLocationFromCache(city)
+    .then(result => {
+      let { rowCount, rows } = result;
+      if (rowCount > 0) {
+        response.send(rows[0])
+      } else {
+        return getLocationFromApi(city, response);
+      }
+
+    })
+}
+
+function getLocationFromApi(city, response) {
   const url = 'https://us1.locationiq.com/v1/search.php';
   superagent.get(url)
     .query({
@@ -76,13 +103,19 @@ function locationHandler(request, response) {
     .then(locationResponse => {
       let geoData = locationResponse.body;
       const location = new Location(city, geoData);
-      response.send(location);
+
+      setLocationInCache(location)
+        .then(() => {
+          response.send(location);
+        });
     })
     .catch(err => {
       console.log(err);
       errorHandler(err, request, response);
     });
 }
+
+
 
 // Route Handler: weather
 function weatherHandler(request, response) {
